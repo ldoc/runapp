cb8fc729bb86e260edf9ee518787f01f69dffc23
ldoc <leandrodelolmo@gmail.com>
2023-02-28 21:08:20 +0100

    Changing default code to start dev

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+// svelte-shim.d.ts
+declare module "*.svelte" {
+    import type { ComponentType } from "svelte";
+    const component: ComponentType;
+    export default component;
+}
